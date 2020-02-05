816ebd3cd4a44908b822e2d0dc748fea7d4f7063
McCabe Northup <mccabenorthup@gmail.com>
2020-02-05 10:09:54 -0700

    dashboard page is functional, slightly skinny with info

--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -1,40 +1,39 @@
 const path = require("path");
 const router = require("express").Router();
 
-// module.exports = function(app) {
 
-//   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-//   // index route loads view.html
-//   router.get("/", function(req, res) {
-//       console.log("yay")
-//     res.sendFile(path.join(__dirname, "./public/test.html"));
-//   });
-// // "/dashboard" loads the specific user dashboard
-// //   app.get("/dashboard", function(req, res) {
-// //     res.sendFile(path.join(__dirname, "../public/user-dashboard.html"));
-// //   });
+  // Each of the below routes just handles the HTML page that the user gets sent to.
 
-// //   // "/account" loads the users info to be updated or reviewed
-// //   app.get("/account", function(req, res) {
-// //     res.sendFile(path.join(__dirname, "../public/account-info.html"));
-// //   });
-
-// //   // "/stream" loads the individual user's streams(messages) dashboard
-// //   app.get("/streams", function(req, res) {
-// //     res.sendFile(path.join(__dirname, "../public/streams.html"));
-// //   });
-
-// //   // "/rafts" loads the individual user's rafts(documents) dashboard
-// //   app.get("/rafts", function(req, res) {
-// //     res.sendFile(path.join(__dirname, "../public/rafts.html"));
-// //   });
-
-// };
-
+  // index route loads view.html
   router.get("/", function(req, res) {
       console.log("yay")
-    res.sendFile(path.join(__dirname, "../../public/test.html"));
-  })
+    res.sendFile(path.join(__dirname, "./public/test.html"));
+  });
+// "/dashboard" loads the specific user dashboard
+//   app.get("/dashboard", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/user-dashboard.html"));
+//   });
+
+//   // "/account" loads the users info to be updated or reviewed
+//   app.get("/account", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/account-info.html"));
+//   });
+
+//   // "/stream" loads the individual user's streams(messages) dashboard
+//   app.get("/streams", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/streams.html"));
+//   });
+
+//   // "/rafts" loads the individual user's rafts(documents) dashboard
+//   app.get("/rafts", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/rafts.html"));
+//   });
+
+
+//   router.get("/", function(req, res) {
+//       console.log("yay")
+//     res.sendFile(path.join(__dirname, "../../public/test.html"));
+//   })
 
   module.exports = router
