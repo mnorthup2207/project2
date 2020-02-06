81d3f3af2f0169a0ebce054a03e626805d46dde1
McCabe Northup <mccabenorthup@gmail.com>
2020-02-06 14:35:09 -0700

    rafts count loads to page

--- routes/api/rafts.js
+++ routes/api/rafts.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const controller = require("../../controllers/raftsController");
 var passport = require("../../config/passport");
 
+router.route("/all")
+    .get(controller.findByUser)
 
 router.route("/rafts")
-    .get(controller.findByUser)
-    .get(controller.findByRaft)
+    // .get(controller.findByRaft)
     .post(controller.uploadRaft)
+    
 module.exports = router
