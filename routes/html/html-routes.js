c1372036a55b4410366f7b39994161d3161327b3
McCabe Northup <mccabenorthup@gmail.com>
2020-02-08 16:20:11 -0700

    merged handlebars back to html files

--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -25,7 +25,7 @@ router.get("/", function (req, res) {
 
 });
 
-router.get("/dashboard", isAuth, function(req, res) {
+router.get("/dashboard", isAuth, function (req, res) {
     res.sendFile(path.join(__dirname, "../../public/dashboard.html"))
 })
 // router.get("/", function (req, res) {
@@ -47,55 +47,22 @@ router.get("/dashboard", isAuth, function(req, res) {
 //         video: newVideo
 //     })
 // });
-
-// // "/dashboard" loads the specific user dashboard
-// router.get("/dashboard", isAuth, function (req, res) {
-//     res.render("dashboard", {
-//         user: req.user
-//     })
-
-// router.get("/dashboard", isAuth, function (req, res) {
-//     res.render(__dirname, "../../public/dashboard.html")
-// });
-
-// router.get("/test", function (req, res) {
-//     res.render("test")
-//     // res.sendFile(path.join(__dirname, "../../public/test.html"));
-// });
-
-// router.get('/loader/rafts', function (req, res) {
-//     res.render("loader", {
-//         rafts: true,
-//     }
-//     );
-// });
-
-// router.get('/rafts', function (req, res) {
-//     res.render(__dirname, "../../public/rafts.html");
-// });
-
-// router.get('/loader/streams', function (req, res) {
-//     res.render("loader", {
-//         streams: true,    
-//     }
-//     );
-// });
-
-// router.get('/streams', function (req, res) {
-//     res.render(__dirname, "../../public/streams.html");
-// });
-// // "/account" loads the users info to be updated or reviewed
-// router.get("/account", isAuth, function (req, res) {
-//     res.render("account");
-// });
-
-
-
-
-
-//   router.get("/", function(req, res) {
-//       console.log("yay")
-//     res.sendFile(path.join(__dirname, "../../public/test.html"));
-//   })
+router.get("/dashboard", isAuth, function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/dashboard.html"));
+});
+router.get('/rafts', function (req, res) {
+    // res.send("hello")
+    res.sendFile(path.join(__dirname, "../../public/rafts.html"));
+});
+router.get('/streams', function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/streams.html"));
+});
+router.get("/account", isAuth, function (req, res) {
+    res.sendFile(path.join(__dirname, "../../public/account.html"));
+});
+router.get("/test", function (req, res) {
+    console.log("yay")
+    res.sendFile(path.join(__dirname, "../../public/test.html"));
+})
 
 module.exports = router;
