b2ab1cbf8ab813263d9be59b7778e014d8351f2a
McCabe Northup <mccabenorthup@gmail.com>
2020-02-08 20:15:24 -0700

    added dynamic fetures to streams page

--- public/js/userStream.js
+++ public/js/userStream.js
@@ -6,22 +6,40 @@ let userArray = [2];
 
 function loadUsers() {
     $.get("/api/user/all", data => {
-        // for (const item of data){
-        //     const first = item.first_name
-        //     const last = item.last_name
-        //     const id = item.id
-        //     $("#eachUser").append(
-        //         `<li>
-        //             <div class="row" id="downstreamUser" data-UserId="">
-        //                 <div class="col-md-7">
-        //                     <h4 class="userTag">${first} ${last}</h4>
-        //                 </div>
-        //             </div>
-        //             <button id="str" value="${id}" data-name="${first} ${last}" class="addUserBtn"><i class="fas fa-user-plus"></i></button>
-        //         </li>`
-        //     )
-        // }
-        console.table("users", data[0]);
+        for (const item of data){
+            const first = item.first_name
+            const last = item.last_name
+            const id = item.id
+            $("#eachUser").append(
+                `<li>
+                    <div class="row" id="downstreamUser" data-UserId="">
+                        <div class="col-md-7">
+                            <h4 class="userTag">${first} ${last}</h4>
+                        </div>
+                    </div>
+                    <button id="str" value="${id}" name="${first} ${last}" class="addUserBtn"><i class="fas fa-user-plus"></i></button>
+                </li>`
+            )
+        }
+        $(".addUserBtn").on("click", function(e) {
+            e.preventDefault();
+            // $("#streamUser").append(`${$(this).val()}`)
+            console.log("clicked");
+            // builds streamUserList to append to the above input
+            streamUserList.push({
+                id: $(this)[0].value,
+                name: $(this)[0].name
+            })
+            console.log(streamUserList);
+            for (const item of streamUserList) {
+                const name = item.name;
+                $("#streamUser").html(name);
+                console.log(`append User ${name}`);
+            }
+
+
+        })
+        console.table("users", data);
     });
 };
 
@@ -29,7 +47,39 @@ function loadUsers() {
 function loadStreams() {
     $.get("/api/user-stream/all", data => {
         // loop throuh streams for left pane
-        console.log("streams data", data);
+        console.log("raw", data);
+        console.log("data.length", data.length);
+
+        // const userArr = data.filter(name => data[i].users[x].first_name && data[i].users[x].last_name)
+
+        for (let i = 0; i < data.length; i++ ) {
+            // console.log(`Name: ${data[i].users[i].first_name} ${data[i].users[i].last_name} IndexNum: ${i}`);
+            // console.log(`indexNum: ${i}`, data[i].users, `users.length`, data[i].users.length);
+            const userArr = [];
+            for (let x = 0; x < data[i].users.length ; x++ ) {
+                const firstName = data[i].users[x].first_name;
+                const lastName = data[i].users[x].last_name;
+
+                userArr.push(`${firstName} ${lastName}`)
+                // console.log(`Data Index: ${i} User Index ${x} Name: ${firstName} ${lastName}`);                
+            }     
+            const finalLoop =[]
+            for (const user of userArr) {
+                finalLoop.push(`${user}`)
+            }
+
+            const looped = finalLoop.toString().split(",").join(", ")
+            console.log("final loop list", finalLoop);
+            console.log(looped);
+            $("#indStreams").append(
+                `<li class="streamId">
+                <h4>${looped}</h4>
+                </li>`
+            )
+            
+            console.log("userArr", userArr);
+        }
+
     });
 };
 
